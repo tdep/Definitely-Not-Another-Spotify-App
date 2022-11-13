814c3b2976b86d0e776018afe02697d4f6436d53
Trevor DePew <t.depewmusic@gmail.com>
2022-11-13 16:45:29 -0500

    first push to main repository

---
+++ keys.js
@@ -0,0 +1,2 @@
+var clientId = '64e2c3a50706426fb249364e1cd0a7df';
+var clientSecret = 'ef7cbf92f3d443ba92c749dedb960a97';
---
+++ main.js
@@ -0,0 +1,11 @@
+const client_id = clientId;
+const client_secret = clientSecret;
+
+const auth = async () => {
+    let res = await fetch("https://accounts.spotify.com/api/token", {
+        method: 'POST',
+        headers: {
+            "Authorization": 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+        },
+    })
+}
