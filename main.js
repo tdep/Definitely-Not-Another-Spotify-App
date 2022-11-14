83f88b22cd3ee9aa911f1c11be3b86d5d72b1cb3
Trevor DePew <t.depewmusic@gmail.com>
2022-11-14 02:27:31 -0500

    API search functionality

--- main.js
+++ main.js
@@ -1,11 +1,43 @@
-const client_id = clientId;
-const client_secret = clientSecret;
+//Global variables
+//Search bar variables
+const searchBar = document.getElementById("search-bar")
+const submitButton = document.getElementById("submit-button")
+const searchForm = document.getElementById("search-form")
+const resultDiv = document.getElementById("search-result")
 
-const auth = async () => {
-    let res = await fetch("https://accounts.spotify.com/api/token", {
-        method: 'POST',
-        headers: {
-            "Authorization": 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-        },
-    })
+
+
+//Search function
+const runSearch = () => {
+	searchForm.addEventListener("submit", (e) => {
+		e.preventDefault()
+		//GET Request from deezerdevs
+		let searchItem = searchForm.search.value
+		let request = async () => {
+			let req = await fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${searchItem}`, {
+				method: 'GET',
+				headers: {
+					'X-RapidAPI-Key': 'a2399ab2a1mshbaea749573e0d46p1c86e7jsn073c23c80e70',
+					'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+				}
+			})
+			let res = await req.json()
+			let entryItem = res.data
+			//render each element from res.data to the DOM
+			entryItem.forEach((entry) => {
+				let trackCard = document.createElement("li")
+				trackCard.id = entry.id
+				trackCard.innerText = `Album: ${entry.album.title}; Artist: ${entry.artist.name}`
+				let trackCardImg = document.createElement("img")
+				trackCardImg.src = entry.album.cover_small
+				resultDiv.append(trackCard, trackCardImg)
+			})
+		}
+		request()
+	})
 }
+
+//function calls
+runSearch()
+
+
