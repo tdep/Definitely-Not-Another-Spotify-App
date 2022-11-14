6f768eee3a6bcbbbb0ee266e9f0be0c9b84b8aec
Trevor DePew <t.depewmusic@gmail.com>
2022-11-14 03:38:56 -0500

    trackCards divs populated, temporary styling

--- main.js
+++ main.js
@@ -3,7 +3,16 @@
 const searchBar = document.getElementById("search-bar")
 const submitButton = document.getElementById("submit-button")
 const searchForm = document.getElementById("search-form")
+//result list variables
 const resultDiv = document.getElementById("search-result")
+const trackCardContents = {
+	trackCardImg:"",
+	trackTitle:"",
+	trackAlbum:"",
+	trackArtist:"",
+	trackDuration:""
+}
+
 
 
 
@@ -25,12 +34,26 @@ const runSearch = () => {
 			let entryItem = res.data
 			//render each element from res.data to the DOM
 			entryItem.forEach((entry) => {
-				let trackCard = document.createElement("li")
-				trackCard.id = entry.id
-				trackCard.innerText = `Album: ${entry.album.title}; Artist: ${entry.artist.name}`
 				let trackCardImg = document.createElement("img")
 				trackCardImg.src = entry.album.cover_small
-				resultDiv.append(trackCard, trackCardImg)
+				let trackTitle = document.createElement("p")
+				trackTitle.innerText = entry.title
+				trackTitle.className = "track-title"
+				let trackAlbum = document.createElement("p")
+				trackAlbum.innerText = entry.album.title
+				trackAlbum.className = "track-album"
+				let trackArtist = document.createElement("p")
+				trackArtist.innerText = entry.artist.name
+				trackArtist.className = "track-artist"
+				let trackDuration = document.createElement("p")
+				trackDuration.innerText = `${entry.duration} seconds`
+				let trackCard = document.createElement("div")
+				trackCard.id = entry.id
+				trackCard.setAttribute("style", "border: 1px solid black; margin:20px auto 10px auto;")
+				
+
+				trackCard.append(trackCardImg, trackTitle, trackAlbum, trackArtist, trackDuration)
+				resultDiv.append(trackCard)
 			})
 		}
 		request()
